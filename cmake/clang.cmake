074975e9ca7464d5e6068a5ebcec8b838085a914
Luke D'Alessandro <ldalessa@iu.edu>
2024-08-11 22:41:58 +0000

    Have all of the presets use the clang toolchain.
    This requires the clang binaries be in the path.

---
+++ cmake/clang.cmake
@@ -0,0 +1,4 @@
+set(CMAKE_CXX_COMPILER "clang++" CACHE FILEPATH "C++ compiler")
+set(CMAKE_C_COMPILER "clang" CACHE FILEPATH "C compiler")
+set(CLANG_TIDY "clang-tidy" CACHE FILEPATH "Clang Tidy binary")
+set(CMAKE_CXX_FLAGS_INIT "-stdlib=libc++" CACHE FILEPATH "Default C++ Flags")
